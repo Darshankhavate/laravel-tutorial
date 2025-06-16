9fe73dc32b2323b36c935e24e5dc4272280f86aa
Darshankhavate <darshankhavate@gmail.com>
2025-06-16 12:38:15 +0530

    http client clear

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,9 +8,5 @@ use Illuminate\Support\Facades\Http;
 class UserController extends Controller
 {
     //
-    function httpclient(){
-        $response=Http::get('https://jsonplaceholder.typicode.com/users/1');
-        $response= $response->body();
-        return view('user',['data'=>json_decode($response)]);
-    }
+    
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -2,23 +2,5 @@
     <h1>User Data</h1>
 
     <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
-     <ul>
-        <li>
-            <span>Name:-</span><span><b>{{$data->name}}</b></span>
-        </li>
-        <li>
-            <span>UserName:-</span><span><b>{{$data->username}}</b></span>
-        </li>
-        <li>
-            <span>email:-</span><span><b>{{$data->email}}</b></span>
-        </li>
-        <li>
-            <span>phone:-</span><span><b>{{$data->phone}}</b></span>
-        </li>
-        <li>
-            <span>website:-</span><span><b>{{$data->website}}</b></span>
-        </li>
-    
-       
-     </ul>
+
 </div>
